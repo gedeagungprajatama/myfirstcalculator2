d271b6b1b16ad9878a88ad932af4342401f348e7
gede agung prajatama <gede.prajatama@students.amikom.ac.id>
2018-09-26 23:29:56 +0700

    my first project

---
+++ AggCalculator/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace LeetCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboOperation = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(129, 69);
+            this.txtNilaiA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(180, 26);
+            this.txtNilaiA.TabIndex = 0;
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(129, 128);
+            this.txtNilaiB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(180, 26);
+            this.txtNilaiB.TabIndex = 1;
+            this.txtNilaiB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Location = new System.Drawing.Point(129, 178);
+            this.txtHasil.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(180, 26);
+            this.txtHasil.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Operasi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nilai B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 189);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hasil";
+            // 
+            // comboOperation
+            // 
+            this.comboOperation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboOperation.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboOperation.FormattingEnabled = true;
+            this.comboOperation.Items.AddRange(new object[] {
+            "Penambahan",
+            "Pengurangan",
+            "Pembagian",
+            "Perkalian"});
+            this.comboOperation.Location = new System.Drawing.Point(129, 14);
+            this.comboOperation.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboOperation.Name = "comboOperation";
+            this.comboOperation.Size = new System.Drawing.Size(180, 28);
+            this.comboOperation.TabIndex = 7;
+            this.comboOperation.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 245);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Hitung";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 317);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboOperation);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "AggCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboOperation;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AggCalculator/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeetCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        private int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        private int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+        private int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Filter supaya progran tidak error apabila di input ""
+            int a = 0;
+            var b = 0;
+            if (txtNilaiA.Text == "")
+            {
+                a = 0;
+            }
+            else {
+                a = int.Parse(txtNilaiA.Text);
+            }
+            if (txtNilaiB.Text == "")
+            {
+                b = 0;
+            }
+            else
+            {
+                b = Convert.ToInt32(txtNilaiB.Text);
+            }
+            //
+
+            // filter supaya apabila belum di set operasi maka muncul pilih operasi.
+            txtHasil.Clear();
+            if (comboOperation.Text == "Penambahan")
+            {
+                txtHasil.Text = Convert.ToString(Penambahan(a, b));
+            }
+            else if (comboOperation.Text == "Pengurangan")
+            {
+                txtHasil.Text = Convert.ToString(Pengurangan(a, b));
+            }
+            else if (comboOperation.Text == "Perkalian")
+            {
+                txtHasil.Text = Convert.ToString(Perkalian(a, b));
+            }
+            else if (comboOperation.Text == "Pembagian")
+            {
+                txtHasil.Text = Convert.ToString(Pembagian(a, b));
+            }
+            else {
+                txtHasil.Text = "maaf belum pilih operasi...";
+            }
+        }
+        //
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
